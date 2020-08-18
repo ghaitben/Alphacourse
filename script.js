315dd86688d7b209df39ad94612a4a645ea39ccd
Ghait <ghaitben2017@gmail.com>
2020-08-18 19:51:38 +0100

    animated NavBar

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// search value is deleted once the search field looses focus
 const searchBar = document.getElementById('INPUT_1');
 
 searchBar.addEventListener('blur', function(){
@@ -6,6 +7,7 @@ searchBar.addEventListener('blur', function(){
 
 });
 
+//the menu on phone is not left open
 const checkbox = document.getElementById('check');
 
 window.addEventListener('resize',function(){
@@ -16,3 +18,43 @@ window.addEventListener('resize',function(){
 
   }
 },true);
+
+
+
+//dynamic naviguation bar ,showing and hiding based on sroll direction
+const navBar = document.getElementsByClassName('secondary')[0];
+
+window.onscroll = function(){
+
+  var pos = window.pageYOffset;
+
+  if(pos > 160 && window.innerWidth > 1100)
+  {
+
+
+    navBar.style.top = "0px";
+  }
+  if(pos < 160 && window.innerWidth > 1100)
+  {
+    //navBar.style.display = "none";
+    navBar.style.top = "-160px";
+
+  }
+};
+
+  //var pos = window.pageYOffset;
+
+  //if (pos > prePos)
+  //{
+  //  console.log("you are scrolling down");
+  //}
+
+  //else
+  //{
+  // console.log("you are scrolling up");
+  //}
+
+  //prePos = pos;
+
+
+//};
