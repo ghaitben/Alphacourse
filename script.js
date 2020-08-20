0e75a4daa7d4604b6bcd3a77f927f8cdeb91edf4
Ghait <ghaitben2017@gmail.com>
2020-08-20 13:09:58 +0100

    Slider with no content and random images

--- script.js
+++ script.js
@@ -41,20 +41,143 @@ window.onscroll = function(){
 
   }
 };
+//javascirpt code for the slider
+const slides = document.querySelectorAll('.slide');
+const next = document.querySelector('#next');
+const prev = document.querySelector('#prev');
+const auto = true;
+const timeInterval = 4000;
+let slideInterval;
+const items = document.querySelectorAll('.items');
+const buttons  = document.querySelectorAll('.slideButton');
+let index = 0;
+items[0].style.backgroundColor = "#ffcc66";
 
-  //var pos = window.pageYOffset;
+const nextSlide = () => {
 
-  //if (pos > prePos)
-  //{
-  //  console.log("you are scrolling down");
-  //}
+  const current = document.querySelector('.current');
 
-  //else
-  //{
-  // console.log("you are scrolling up");
-  //}
-
-  //prePos = pos;
+  for(let i = 0 ; i < slides.length ; i++)
+  {
+    if(current === slides[i])
+    {
+      index = i;
+    }
+  }
 
 
-//};
+  current.classList.remove('current');
+
+  if(current.nextElementSibling)
+  {
+    current.nextElementSibling.classList.add('current');
+
+  }
+  else
+  {
+    slides[0].classList.add('current');
+  }
+  items.forEach(element => {
+    element.style.backgroundColor = "#ccc";
+  });
+  items[(index+1)%3].style.backgroundColor = "#ffcc66";
+  setTimeout(() => current.classList.remove('current'));
+
+}
+
+
+
+
+buttons[0].addEventListener('click', e => {
+
+  // turn all buttons to grey
+  items.forEach(element => {
+
+    element.style.backgroundColor = "#ccc";
+  });
+
+  //color the right btutton in #ffcc66
+
+  items[0].style.backgroundColor = "#ffcc66";
+
+  //slide movement
+
+  const current = document.querySelector('.current');
+
+  current.classList.remove('current');
+
+  slides[0].classList.add('current');
+  if(auto)
+  {
+    clearInterval(slideInterval);
+    slideInterval = setInterval(nextSlide , timeInterval);
+  }
+
+});
+
+
+
+buttons[1].addEventListener('click', e => {
+
+  // turn all buttons to grey
+  items.forEach(element => {
+
+    element.style.backgroundColor = "#ccc";
+  });
+
+  //color the right btutton in #ffcc66
+
+  items[1].style.backgroundColor = "#ffcc66";
+
+  //slide movement
+
+  const current = document.querySelector('.current');
+
+  current.classList.remove('current');
+
+  slides[1].classList.add('current');
+
+  if(auto)
+  {
+    clearInterval(slideInterval);
+    slideInterval = setInterval(nextSlide , timeInterval);
+
+  }
+
+});
+
+
+
+
+buttons[2].addEventListener('click', e => {
+
+  // turn all buttons to grey
+  items.forEach(element => {
+
+    element.style.backgroundColor = "#ccc";
+  });
+
+  //color the right btutton in #ffcc66
+
+  items[2].style.backgroundColor = "#ffcc66";
+
+  //slide movement
+
+  const current = document.querySelector('.current');
+
+  current.classList.remove('current');
+
+  slides[2].classList.add('current');
+  if(auto)
+  {
+    clearInterval(slideInterval);
+    slideInterval = setInterval(nextSlide , timeInterval);
+
+  }
+
+});
+
+if(auto)
+{
+  slideInterval = setInterval(nextSlide , timeInterval);
+}
